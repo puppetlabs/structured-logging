8970679afb1a42ff780d71533ae4bfb448522b7c
Russell Mull <russell.mull@puppetlabs.com>
2015-09-29 10:22:54 -0700

    Release automation support

--- project.clj
+++ project.clj
@@ -8,4 +8,11 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/core.match "0.2.2"]
                  [cheshire "5.4.0"]
-                 [org.slf4j/slf4j-api "1.7.12"]])
+                 [org.slf4j/slf4j-api "1.7.12"]]
+  :plugins [[lein-release "1.0.5"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]]
+  :lein-release {:scm :git
+                 :deploy-via :lein-deploy})
