09f8888381b00f0be702454fcf00fb56a1b58049
Rob Browning <rlb@puppet.com>
2016-11-04 10:16:19 -0500

    (PDB-2908) Generate all log messages via callback
    Get out of the message formatting business.
    
    Drop all built-in formatting, including {item} map value expansions and
    format-style expansions.  Drop logp and logf.
    
    Change maplog to require a message formatter as the last argument, and
    generate the log message by passing the structured data to the
    formatter.
    
    This provides a general way to support arbitrary formatters, including
    some like those in https://github.com/puppetlabs/clj-i18n, which require
    that their invocations appear literally, at each translation call site,
    i.e.:
    
        (maplog :info {:temperature t :stamp (now)}
                #(i18n/trs "Temperature has exceeded {0} at {1}"
                           (:temperature %) (:stamp %)))

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://github.com/puppetlabs/structured-logging"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.logstash.logback/logstash-logback-encoder "4.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.logstash.logback/logstash-logback-encoder "4.6"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/core.match "0.2.2"]
-                 [cheshire "5.4.0"]
-                 [org.slf4j/slf4j-api "1.7.12"]]
+                 [cheshire "5.6.3"]
+                 [org.slf4j/slf4j-api "1.7.21"]]
   :plugins [[lein-release "1.0.5"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_jenkins_username
--- test/puppetlabs/structured_logging/core_test.clj
+++ test/puppetlabs/structured_logging/core_test.clj
@@ -1,32 +1,11 @@
 (ns puppetlabs.structured-logging.core-test
   (:require [clojure.test :refer :all]
+            [clojure.tools.logging :refer [*logger-factory*]]
+            [clojure.tools.logging.impl :as impl]
             [puppetlabs.structured-logging.core :refer :all]
-            [puppetlabs.structured-logging.protocols :refer :all]
-            [clojure.test :refer :all]
-            [clojure.tools.logging :as tlog]
-            [clojure.tools.logging.impl :as impl]))
+            [puppetlabs.structured-logging.protocols :refer :all]))
 
 (def ^:private stns *ns*)
-
-(deftest interleave-all-test
-  (are [a b result] (= result (interleave-all a b))
-    [] [] []
-    [1] [] [1]
-    [] [:a] [:a]
-    [1] [:a] [1 :a]
-    [1 2] [:a] [1 :a 2]
-    [1] [:a :b] [1 :a :b]
-    [1 2] [:a :b] [1 :a 2 :b]))
-
-(deftest interpolate-message-test
-  (are [message ctx-map result] (= result (interpolate-message message ctx-map))
-    "{a}" {:a 1} "1"
-    "before {a}" {:a 1} "before 1"
-    "{a} after" {:a 1} "1 after"
-    "before {a} after" {:a 1} "before 1 after"
-    "first {a} then {b}" {:a 1, :b 2} "first 1 then 2"
-    "first {a} then {b} end" {:a 1, :b 2} "first 1 then 2 end"
-    "kw {key}" {:key :val} "kw :val"))
 
 (defn make-log-record [logger-ns level ex message marker]
   (letfn [(maybe-add-exception [rec]
@@ -54,55 +33,20 @@
 
 (defn expect-log [f expected-log]
   (let [log (atom [])]
-    (binding [tlog/*logger-factory* (atom-logger-factory log)]
+    (binding [*logger-factory* (atom-logger-factory log)]
       (f))
     (is (= expected-log @log))))
 
 (def throwable (Exception. "ex"))
 
-(deftest specify-ns-explicity
-  (testing "custom logp"
-   (are [f expected] (expect-log f expected)
-     #(logp :error "Test" 123)
-     [{:ns stns, :level :error, :message "Test 123"}]
-
-     #(logp [:sync :error] "Test" 123)
-     [{:ns "sync", :level :error, :message "Test 123"}]
-
-     #(logp [:sync :error] throwable "Test" 123)
-     [{:ns "sync", :level :error, :message "Test 123", :throwable throwable}]))
-
-  (testing "custom logf"
-   (are [f expected] (expect-log f expected)
-     #(logf :error "Test %s" 123)
-     [{:ns stns, :level :error, :message "Test 123"}]
-
-     #(logf [:sync :error] "Test %s" 123)
-     [{:ns "sync", :level :error, :message "Test 123"}]
-
-     #(logf [:sync :error] throwable "Test %s" 123)
-     [{:ns "sync", :level :error, :message "Test 123", :throwable throwable}])))
-
-(deftest maplog-test
+(deftest maplog-behavior
   (are [f expected] (expect-log f expected)
-    #(maplog :error {:key :val} "Test")
-    [{:ns stns, :level :error, :message "Test", :marker {:key :val}}]
+       #(maplog :error {:x 1} (fn [m] (str "Test " (:x m))))
+       [{:ns stns, :level :error, :message "Test 1" :marker {:x 1}}]
 
-    #(maplog [:sync :error] {:key :val} "Test")
-    [{:ns "sync", :level :error, :message "Test", :marker {:key :val}}]
+       #(maplog [:sync :error] {:x 2} (fn [m] (str "Test " (:x m))))
+       [{:ns "sync", :level :error, :message "Test 2" :marker {:x 2}}]
 
-    #(maplog [:sync :error] {:key :val} "Test, {key}")
-    [{:ns "sync", :level :error, :message "Test, :val", :marker {:key :val}}]
-
-    #(maplog [:sync :error] {:key :val} "Test, {key} %s" 42)
-    [{:ns "sync", :level :error, :message "Test, :val 42", :marker {:key :val}}]))
-
-(deftest maplog-format-escaping
-  (let [ctx {:w "%foo"
-             :x "%"
-             :y "%%"
-             :z "%%%"}]
-    (expect-log #(maplog :info ctx "%s {w} {x} {y} {z}" "embedded")
-                [{:ns stns :level :info
-                  :message "embedded %foo % %% %%%"
-                  :marker ctx}])))
+       #(maplog [:sync :error] throwable {:x 3} (fn [m] (str "Test " (:x m))))
+       [{:ns "sync", :level :error, :message "Test 3", :throwable throwable
+         :marker {:x 3}}]))
